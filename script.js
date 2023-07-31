cc61bbdbeb50d7f20cc8bc5308f7bedeff2b9448
mejrakucevic <mejrakucevic420@gmail.com>
2023-07-31 17:48:30 +0200

    .

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHous = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekdays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 
-// Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -12,32 +23,29 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  }, 
+  // ES6 enhanced object literals
+ // openingHours,
 
-
-  order: function(starterIndex, mainIndex) {
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function(starterIndex = 1, mainIndex = 0, time= '20:00', adress) {
-    console.log(`Order received ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`);
-  }
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 
+  orderPasta(ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
 
-  
+  orderPizza(mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
 restaurant.orderDelivery({
@@ -76,15 +84,15 @@ console.log(pizza, Risotto, otherFood);
 
 // Objects
 
-const {fri, ...otherDays} = restaurant.openingHours
-console.log(otherDays);
+//const {fri, ...otherDays} = restaurant.openingHours
+//console.log(otherDays);
 
 // 2.functions
 
 const add = function(...numbers) {
   let sum = 0;
   for(let  i=0; i < numbers.length; i++) sum += numbers[i];
-  console.log(sum);
+  // console.log(sum);
 
 }
 
@@ -100,7 +108,64 @@ const x = [23, 5, 7];
 // console.log(x);
 
 
-// USE ANY DATA TYPE, return any data type, short-circuiting
-console.log(3 || 'jONAS'); // returns 3
+// USE ANY DATA TYPE, return any data type, short-circuiting (use the first truthy value)
+// console.log(3 || 'jONAS'); // returns 3
 
-console.log('' || 'Jonas'); // returns jonas
+// console.log('' || 'Jonas'); // returns jonas
+
+// console.log(true || 0); // returns true
+
+// console.log(undefined || null); // returns null
+
+console.log('-------OR---------');
+
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10
+console.log(guests);
+
+
+// nullish values, null and undefined (not 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
+console.log('--------AND-------'); // opposite of OR, returns falsy value, or the last truthy value, //
+//everthing needs to be truthy, as soon as theres something false, it will return the false value.
+
+// console.log(0 && 'Mejra');
+// console.log(7 && 'Mejra');
+
+// console.log('Hello' && 23 && null && 'mejra');
+
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza("mushrooms", 'spinach');
+// }
+
+// restaurant.orderPizza && restaurant.orderPizza 
+// ('mushrooms', 'spinach')
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovani Govannios',
+};
+
+// OR assignment operator
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests = rest1.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// nullish asignemnt operator, allows 0
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+console.log(rest1);
+console.log(rest2);
+

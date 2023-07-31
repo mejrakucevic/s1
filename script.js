b61a94d5b050880c3f3465becf571fee6b8ed3b9
mejrakucevic <mejrakucevic420@gmail.com>
2023-07-31 17:19:39 +0200

    .

--- script.js
+++ script.js
@@ -35,6 +35,9 @@ const restaurant = {
   orderDelivery: function(starterIndex = 1, mainIndex = 0, time= '20:00', adress) {
     console.log(`Order received ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${adress} at ${time}`);
   }
+
+
+  
 };
 
 restaurant.orderDelivery({
@@ -45,21 +48,59 @@ restaurant.orderDelivery({
 });
 
 const {name, openingHours, categories} = restaurant;
-console.log(name, openingHours, categories);
+// console.log(name, openingHours, categories);
 
 // default values
-const {name : restaurantName, openingHours: hours, categories: tags} = restaurant;
-console.log(restaurantName, hours, tags);
+//const {name : restaurantName, openingHours: hours, categories: tags} = restaurant;
+//console.log(restaurantName, hours, tags);
 
-const { menu = [], starterMenu: starters = []} = restaurant;
-console.log(menu, starters);
+//const { menu = [], starterMenu: starters = []} = restaurant;
+//console.log(menu, starters);
 
-// Mutating variables
 
-let a = 112;
-let b = 999;
-const obj = {a: 23, b: 7, c:14};
+// iterables : arrays, strings, maps, sets. NOT objects
 
-({a, b} = obj);
-console.log(a, b);
 
+// 1.desctructiring
+
+// SPREAD, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, because on LEFT side of =
+const [a, b, ...stuff] = [1, 2, 3, 4, 5];
+console.log(a, b, stuff);
+
+
+const [pizza, , Risotto, ...otherFood] = [...restaurant.mainMenu,  ...restaurant.starterMenu];
+console.log(pizza, Risotto, otherFood);
+
+// Objects
+
+const {fri, ...otherDays} = restaurant.openingHours
+console.log(otherDays);
+
+// 2.functions
+
+const add = function(...numbers) {
+  let sum = 0;
+  for(let  i=0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+
+}
+
+add(2, 3)
+add(5, 3, 7, 2)
+add(8, 2, 5, 3, 2, 1, 4, 4)
+
+
+
+const x = [23, 5, 7];
+// add(...x);
+
+// console.log(x);
+
+
+// USE ANY DATA TYPE, return any data type, short-circuiting
+console.log(3 || 'jONAS'); // returns 3
+
+console.log('' || 'Jonas'); // returns jonas

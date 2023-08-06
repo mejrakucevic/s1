bca326da0db42df1417a057e7710d78d57351422
mejrakucevic <mejrakucevic420@gmail.com>
2023-08-06 19:39:22 +0200

    .

--- script.js
+++ script.js
@@ -58,62 +58,107 @@ console.log(restaurant.openingHours?.mon?.open);
 
 const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-for (const day of days) {
-  console.log(day);
-  const open = restaurant.openingHours[day]?.open  ?? 'closed';
-  console.log(`On ${day}, we open at ${open}`);
-}
-
-console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
-
-const users = [
-  {name: 'Jonas', email: 'email@gmail.com'}
-];
-
-console.log(users[0]?.name ?? 'user array empty');
-
-// property names
-const properties = Object.keys(openingHours);
-console.log(properties);
-
-let openStr = `We are open on ${properties.length} days; `;
-
-for (const day of properties) {
-  openStr += `${day}`; 
-console.log(openStr) }
-
-// property values
-// const values = Object.values(openingHours)
-// console.log(values); 
-
-// // Entire object
-// const entires = Object.entries(openingHours);
-
-// for (const x of entries) {
-//   console.log(x);
+// for (const day of days) {
+//   console.log(day);
+//   const open = restaurant.openingHours[day]?.open  ?? 'closed';
+//   console.log(`On ${day}, we open at ${open}`);
 // }
+
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+
+// const users = [
+//   {name: 'Jonas', email: 'email@gmail.com'}
+// ];
+
+// console.log(users[0]?.name ?? 'user array empty');
+
+// // property names
+// const properties = Object.keys(openingHours);
+// console.log(properties);
+
+// let openStr = `We are open on ${properties.length} days; `;
+
+// for (const day of properties) {
+//   openStr += `${day}`; 
+// console.log(openStr) }
+
+// // property values
+// // const values = Object.values(openingHours)
+// // console.log(values); 
+
+// // // Entire object
+// // const entires = Object.entries(openingHours);
+
+// // for (const x of entries) {
+// //   console.log(x);
+// // }
 
 const ordersSet = new Set(['Pasta', 'Pizza', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
 console.log(ordersSet);
 
-console.log(ordersSet.has('Pizza'));
+// // console.log(ordersSet.has('Pizza'));
 
-ordersSet.add('garlic bread');
-ordersSet.delete('Pizza');
-// ordersSet.clear(); // clears all elements
+// ordersSet.add('garlic bread');
+// ordersSet.delete('Pizza');
+// // ordersSet.clear(); // clears all elements
 
-for (const order of ordersSet)
-console.log(order);
+// for (const order of ordersSet)
+// console.log(order);
 
 // Example 
-// const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
-// const staffUnique = [...new Set(staff)];
-// console.log(staffUnique);
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
 
 
-const PlitzerAuthors = ['James Joyce', 'Sally Rooney', 'Sylvia Plath', 'Dostoevsky', 'James Joyce'];
+// const PlitzerAuthors = ['James Joyce', 'Sally Rooney', 'Sylvia Plath', 'Dostoevsky', 'James Joyce'];
 
-const uniquePA = [...new Set(PlitzerAuthors)];
-console.log(uniquePA);
+// const uniquePA = [...new Set(PlitzerAuthors)];
+// console.log(uniquePA);
 
-console.log(new Set('jonasschmedtmann').size); // string count
+// console.log(new Set('jonasschmedtmann').size); // string count
+
+// MAPS
+let resta = new Map();
+
+resta.set('name', 'Classico Italiano');// adding new elements
+resta.set(1, 'Firenze, Italy');
+console.log(resta.set(2, 'Lisbon, Portugal'));
+
+resta
+.set('categories', ['Italian', 'Pizzeria']) 
+.set('open', 11) 
+.set('close', 23)
+.set(true, 'We are open!')
+.set(false, 'We are closed!')
+
+console.log(resta.get('name'));
+
+const time = 18;
+
+resta.get(time > resta.get('open') && time < resta.get('close')) 
+
+let agents = new Map();
+
+agents.set
+  ('name', 'Jett')
+.set
+  ('role', 'duelist')
+.set
+  ('country', 'south korea')
+.set
+  ('age', 21)
+
+
+agents.get(time < agents.get('age'));
+
+console.log(agents.has('country'));
+agents.delete(2);
+agents.set([1, 2], 'Test');
+
+console.log(agents);
+
+console.log(agents.size);
+// console.log(agents.clear)
+
+
